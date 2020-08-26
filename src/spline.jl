a129bd856143b0307c51cb6971a517c42d29adb2
Sebastian Stock <42280794+sostock@users.noreply.github.com>
2020-08-26 15:27:59 +0200

    Simplify docs

--- src/spline.jl
+++ src/spline.jl
@@ -160,22 +160,22 @@ support(s::Spline) = support(basis(s))
 support(b::BSpline) = (t = knots(basis(b)); i = coeffs(b).index; (t[i], t[i+order(b)]))
 
 """
-    splinevalue(spline::Spline, x, [::Derivative{N}]; kwargs...)
+    splinevalue(spline::Spline, x[, ::Derivative{N}]; kwargs...)
 
 Calculate the value of `spline`, or its `N`-th derivative, at `x`.
 
 Two optional keyword arguments can be used to increase performance:
-* `leftknot`: If the index of the relevant interval (i.e.,
-  `intervalindex(basis(spline), x)`) is already known, it can be supplied with the
-  `leftknot` keyword.
 * `workspace`: By default, the function allocates a vector of length `order(spline)` in
   which the calculation is performed. To avoid this, a pre-allocated vector can be supplied
   with the `workspace` keyword. In this case, the returned value is always of type
   `eltype(workspace)`.
+* `leftknot`: If the index of the relevant interval (i.e.,
+  `intervalindex(basis(spline), x)`) is already known, it can be supplied with the
+  `leftknot` keyword.
 
 Instead of calling `splinevalue`, a spline object can be called directly:
-`spline(x, [deriv]; kwargs...)` is equivalent to
-`splinevalue(spline, x, [deriv]; kwargs...)`.
+`spline(x[, Derivative(N)]; kwargs...)` is equivalent to
+`splinevalue(spline, x[, Derivative(N)]; kwargs...)`.
 
 # Examples
 
