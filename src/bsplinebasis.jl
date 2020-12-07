b61ede2530c83c0b829a1eb2d4ee8a59b89f886f
Sebastian Stock <42280794+sostock@users.noreply.github.com>
2020-12-07 09:41:25 +0100

    Add clarification about k to docs (#21)

--- src/bsplinebasis.jl
+++ src/bsplinebasis.jl
@@ -7,6 +7,9 @@ Here, a B-spline basis is completely specified by its order ``k`` and breakpoint
 The knot sequence is derived from the breakpoint sequence by duplicating the first and last
 breakpoints so they each appear ``k`` times. Knot sequences where the first and last
 breakpoints do not appear ``k`` times are not supported by this data type.
+
+!!! note
+    Here, unlike in some other B-spline libraries, ``k`` always refers to the *order* of a B-spline, not its *degree* (order = degree + 1).
 """
 struct BSplineBasis{T<:AbstractVector{<:Real}}
     order::Int
